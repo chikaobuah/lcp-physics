28470d35175bd717e922981b1b89c2d38772435f
Filipe De Avila Belbute Peres <filipeabperes@gmail.com>
2017-12-07 23:44:27 -0800

    Added placeholder test

---
+++ tests/test_placeholder.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testDemo(self):
+        pass
+
+
+if (__name__ == '__main__'):
+     unittest.main()
